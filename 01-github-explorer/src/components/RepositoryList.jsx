5a2fad7a9b90538794f1be58d64c06fb2db8bc38
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-09 16:07:07 -0300

    feat: add useEffect in RepositoryList

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react'
 import{ RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
+
 
 
 const repository = {
@@ -10,6 +12,13 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, [])
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>

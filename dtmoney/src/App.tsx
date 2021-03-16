fc1b5c1452a71ad9f24684a9ef33a627e2ecb4f6
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-16 14:37:41 -0300

    styles: add global.ts

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { GlobalStyle } from './assets/styles/global';
 
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }

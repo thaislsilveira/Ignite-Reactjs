839846551e5add293ca00c7de5523031d507d6b5
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-18 19:41:21 -0300

    style: change colors button

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
 
@@ -61,6 +61,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#e52e4D'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -68,7 +74,10 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#ccc' : 'trasparent'};
+  background: ${(props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor]) 
+    : 'trasparent'
+  };
 
   display: flex;
   align-items: center;

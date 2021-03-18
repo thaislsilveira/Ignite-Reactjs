3400f4a24d6bf7bae122655536444b0a02259100
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-18 19:34:23 -0300

    style: change style RadioBox

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -57,38 +57,42 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
 
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
+  background: ${(props) => props.isActive ? '#ccc' : 'trasparent'};
 
-    &:hover {
-      border-color: ${darken( 0.1, '#d7d7d7' )};
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+  transition: border-color 0.2s;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
-
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-
-    }
+  &:hover {
+    border-color: ${darken( 0.1, '#d7d7d7' )};
   }
 
-`
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }  
+
+`;
 
 

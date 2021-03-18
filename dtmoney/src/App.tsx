a4ebfac5f8e2d7094496a8d70ce19225f9bdd601
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-18 08:37:10 -0300

    fix: transfer modal to newTransactionModal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
 import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './assets/styles/global';
 
@@ -25,12 +27,11 @@ function App() {
 
      <Dashboard />
 
-     <Modal 
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+     <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen}
+     onRequestClose={handleCloseNewTransactionModal}
+     />
+
       <GlobalStyle />
     </div>
   );

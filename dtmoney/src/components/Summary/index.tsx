28c3789767a351d7e81b2cf0353c0c95417b3524
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-19 15:42:50 -0300

    fix: change summary

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -10,7 +10,33 @@ export function Summary() {
 
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  // const totalDeposit = transactions.reduce((acc, transaction) => {
+  //   if(transaction.type === 'deposit') {
+  //     return acc + transaction.amount;
+  //   }
+
+  //   return acc;
+  // }, 0)
+
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if(transaction.type === 'deposit') {
+
+      acc.deposit += transaction.amount;
+      acc.total += transaction.amount;
+
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+
+  }, {
+    deposit: 0,
+    withdraws: 0,
+    total: 0,
+  })
 
   return (
     <Container>
@@ -19,21 +45,36 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+          }).format(summary.deposit)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -24,7 +24,7 @@ createServer({
           title: 'Aluguel',
           type: 'withdrow',
           category: 'Casa',
-          amount: 1100,
+          amount: -1100,
           createdAt: new Date('2021-02-14 11:00:00'),
         }
       ]

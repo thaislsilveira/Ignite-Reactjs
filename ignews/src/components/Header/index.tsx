6833cbb07fbd951bc7f247ac332cbb470329003d
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-22 17:27:43 -0300

    feat: add SignInButton

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from '../SignIntButton'
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -9,6 +10,8 @@ export function Header() {
           <a className={styles.active} href="">Home</a>
           <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
 
     </header>

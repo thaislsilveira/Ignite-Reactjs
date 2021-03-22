fe4e65ada46927ac59bd11e8c261d55d185fefcb
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-22 16:50:58 -0300

    style: add global styles

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
     <Head>
       <title>Início | ig.news</title>
     </Head>
-    <h1 className={styles.title}>
+    <h1>
       Hello World
     </h1>
     </>

ea2f87c807f150b7f281ccbb7d58a21abfd0e28b
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-22 16:37:18 -0300

    Chore: add  Roboto font

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styles from  '../styles/home.module.scss';
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
     <h1 className={styles.title}>
       Hello World
     </h1>
+    </>
   )
 }

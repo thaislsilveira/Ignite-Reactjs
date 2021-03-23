7e040d7b246cd66dd8a7ddfb2d3f610e9f628afb
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-23 08:34:12 -0300

    feat: add next-auth

--- ignews/src/components/SignIntButton/index.tsx
+++ ignews/src/components/SignIntButton/index.tsx
@@ -1,20 +1,30 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import {signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
+  const [session] = useSession()
 
-  const isUserLoggedIn = true;
+  console.log(session)
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#84d361" />
-      Thaís Silveira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+      <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+      >
       <FaGithub color="#eba417" />
       SignIn with Github
     </button>

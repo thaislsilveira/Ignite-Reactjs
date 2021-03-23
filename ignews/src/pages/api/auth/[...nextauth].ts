0e11840afd059599476adf5a3a7df0280bb53253
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-23 09:59:56 -0300

    fix: Checking for duplicate user

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -14,18 +14,31 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('User_by_email'),
+                  q.Casefold(user.email)
+               )
+             )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+               q.Match(
+                  q.Index('User_by_email'),
+                  q.Casefold(user.email)
+               )
+            )
           )
         )
 

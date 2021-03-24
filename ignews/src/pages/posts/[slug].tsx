8041e249197914ecf9df38a250ac9aa857c9fc5d
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-24 17:58:52 -0300

    fix: Page: Post preview

--- ignews/src/pages/posts/[slug].tsx
+++ ignews/src/pages/posts/[slug].tsx
@@ -44,7 +44,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   console.log(session)
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: `/`,

36b674af2f79d9723e55ce1fcff029577aeb0459
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-03 23:11:37 -0300

    chore: configure fonts

--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../styles/theme';
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- dashgo/src/styles/theme.ts
+++ dashgo/src/styles/theme.ts
@@ -1,19 +1,24 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors: {
     gray: {
-      "900": "#181B23",
-      "800": "#1F2029",
-      "700": "#353649",
-      "600": "#4B4D63",
-      "500": "#616480",
-      "400": "#797D9A",
-      "300": "#9699D0",
-      "200": "#B3B5C6",
-      "100": "#D1D2DC",
-      "50": "#EEEEF2",
+      '900': '#181B23',
+      '800': '#1F2029',
+      '700': '#353649',
+      '600': '#4B4D63',
+      '500': '#616480',
+      '400': '#797D9A',
+      '300': '#9699D0',
+      '200': '#B3B5C6',
+      '100': '#D1D2DC',
+      '50': '#EEEEF2'
     }
+  },
+
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
   },
   styles: {
     global: {

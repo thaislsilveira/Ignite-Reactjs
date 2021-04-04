7a10f77eb7eb930e8a77d8ae68e9a2eeda101cb1
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-04 10:15:04 -0300

    fix: change Header

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -40,6 +40,36 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Thaís Silveira</Text>
+            <Text color="gray.300" fontSize="small">
+              thais_lsilveira@hotmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Thaís Silveira"
+            src="https://avatars.githubusercontent.com/u/34002389?v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )

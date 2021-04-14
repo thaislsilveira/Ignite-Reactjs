14afd6db30173cb6bd3cb8f17ba654ae14c98b46
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-14 16:20:02 -0300

    chore: Configurando DevTools

--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }

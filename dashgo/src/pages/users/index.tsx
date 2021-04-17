3d04f3f271d963cc083e3bb14aa1daeedb8c9852
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-17 10:33:18 -0300

    fix: Signaling data refetch

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, isFetching } = useQuery(
     'users',
     async () => {
       const response = await fetch('http://localhost:3000/api/users')
@@ -65,6 +65,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button

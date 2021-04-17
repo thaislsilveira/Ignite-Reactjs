77b1b1bab1c217d5ceb0226a626817499cab5e5b
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-04-17 10:52:53 -0300

    fix: creating list hook

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -16,39 +16,16 @@ import {
   Spinner
 } from '@chakra-ui/react'
 import Link from 'next/link'
-import { useQuery } from 'react-query'
+
 import { RiAddLine } from 'react-icons/ri'
 
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
-import { api } from '../../services/api'
+import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('/users')
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          })
-        }
-      })
-
-      return users
-    },
-    {
-      staleTime: 100 * 5 // 5 seconds
-    }
-  )
+  const { data, isLoading, error, isFetching } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,

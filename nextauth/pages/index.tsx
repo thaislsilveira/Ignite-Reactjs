d941d5910c376026d1d96779df53503cb8241be7
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-11 07:54:03 -0300

    chore: add prettier and eslint

--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <h1>Hello World</h1>
+    <div className={styles.container}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+    </div>
   )
 }

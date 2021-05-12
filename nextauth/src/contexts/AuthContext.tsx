690b93b476d9767ae2ba9552238522e2730cfb1d
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-12 10:46:11 -0300

    feat: saving tokens in cookies

--- nextauth/src/contexts/AuthContext.tsx
+++ nextauth/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useState } from 'react'
+import { setCookie } from 'nookies'
 import Router from 'next/router'
 import { api } from '../services/api'
 
@@ -37,6 +38,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
 
       const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
 
       setUser({
         email,

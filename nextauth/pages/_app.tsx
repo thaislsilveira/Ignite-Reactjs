fff915eaaeb522d4fe1edbe03f271ceeedbe546a
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-12 09:32:29 -0300

    feat: authentication context

--- nextauth/pages/_app.tsx
+++ nextauth/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 
 export default function Home() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
-    <div className={styles.container}>
+    <form onSubmit={handleSubmit}>
       <input
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-    </div>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
   )
 }

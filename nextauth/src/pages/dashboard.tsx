a8f7dcdf8cac2566f37736d5e964cad4fd3ef4f8
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-14 20:04:47 -0300

    feat: creating permission hook

--- nextauth/src/pages/dashboard.tsx
+++ nextauth/src/pages/dashboard.tsx
@@ -1,11 +1,16 @@
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
+import { useCan } from '../hooks/useCan'
 import { setupAPIClient } from '../services/api'
 import { api } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
 
   useEffect(() => {
     api
@@ -14,7 +19,12 @@ export default function Dashboard() {
       .catch((err) => console.log(err))
   }, [])
 
-  return <h1>Dashboard: {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {

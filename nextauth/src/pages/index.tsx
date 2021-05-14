34d05b9979cd9f7cb1aceb8171c900075a53a820
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-14 12:17:11 -0300

    feat: validating visitor

--- nextauth/src/pages/index.tsx
+++ nextauth/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { parseCookies } from 'nookies'
 import { AuthContext } from '../contexts/AuthContext'
 
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -39,19 +40,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+})

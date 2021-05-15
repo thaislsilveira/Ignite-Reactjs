77f2b27b0bf805b1e45b2152f2827455b06367b0
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-15 10:42:13 -0300

    feat: creating permission component

--- nextauth/src/pages/dashboard.tsx
+++ nextauth/src/pages/dashboard.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from 'react'
+import { Can } from '../components/Can'
 import { AuthContext } from '../contexts/AuthContext'
-import { useCan } from '../hooks/useCan'
 import { setupAPIClient } from '../services/api'
 import { api } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
-
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list']
-  })
 
   useEffect(() => {
     api
@@ -22,7 +18,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas1</div>
+      </Can>
     </>
   )
 }

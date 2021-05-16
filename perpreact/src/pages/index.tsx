c536bf667b5dc8f20bc94af1085181c036889f87
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-16 16:33:05 -0300

    feat: application example

--- perpreact/src/pages/index.tsx
+++ perpreact/src/pages/index.tsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { SearchResults } from '../components/SearchResults'
 
 export default function Home() {
-  const [search, setSearch] = useState()
-  //03:44
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if (!search.trim()) {
+      return
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    setResults(data)
+  }
   return (
     <div>
       <h1>Search</h1>
 
       <form onSubmit={handleSearch}>
-        <input type="text" />
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
       </form>
+
+      <SearchResults results={results} />
     </div>
   )
 }

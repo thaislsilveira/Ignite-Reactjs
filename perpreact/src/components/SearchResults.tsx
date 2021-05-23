887edba8e42ea3300e5917d7c042d37302e1ff74
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-23 09:47:24 -0300

    feat: list virtualization

--- perpreact/src/components/SearchResults.tsx
+++ perpreact/src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from './ProductItem'
 
 interface SearchResultsProps {
@@ -16,19 +17,28 @@ export function SearchResults({
   results,
   onAddToWishList
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-            //04:17
-          />
-        )
-      })}
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }

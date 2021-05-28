6c19e2ce29f44fcb7b96c52405f1f5fe156fae31
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-28 08:57:48 -0300

    test: add test Header

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -6,5 +6,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   transform: {
     '^.*\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(scss|css/sass)$': 'identity-obj-proxy'
   }
 }
---
+++ ignews/src/components/Header/Header.spec.tsx
@@ -0,0 +1,29 @@
+import { screen, render } from '@testing-library/react'
+import { Header } from '.'
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+})
+
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+})
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    render(<Header />)
+
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Post')).toBeInTheDocument()
+  })
+})

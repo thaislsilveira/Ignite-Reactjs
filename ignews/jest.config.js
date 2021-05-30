a7ed9090121f3d1376fa0f4852505e899e45f0a2
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-05-30 16:40:43 -0300

    test: coverage report

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)',
+    '!src/**/_app.tsx',
+    '!src/**/_document.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   transform: {
     '^.*\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
